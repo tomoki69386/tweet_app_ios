34a16257b2d6fafedd7225ff6fd6262844d7822e
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-10-26 14:13:44 +0900

    Update

--- tweet/View/RegistrationViewController.swift
+++ tweet/View/RegistrationViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class RegistrationViewController: UIViewController {
     
@@ -15,10 +17,10 @@ class RegistrationViewController: UIViewController {
     @IBOutlet weak var pass2TextField: InputField!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var RegistrationButton: UIButton!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         navigationItem.title = "新規登録"
         setUI()
     }
@@ -45,12 +47,46 @@ class RegistrationViewController: UIViewController {
         label.text = ""
         label.font = UIFont.systemFont(ofSize: 15)
         
-        view.backgroundColor = .bg    }
+        view.backgroundColor = .bg
+    }
+    
+    @IBAction private func Registration(_ sender: UIButton) {
+        guard let email = mailTextField.text else {return}
+        guard let pass = pass1TextField.text else {return}
+        if !errorMessage().1 {
+            label.text = errorMessage().0
+            return
+        }
+    }
+    
+    private func errorMessage() -> (String, Bool) {
+        guard let email = mailTextField.text else {return ("", false)}
+        guard let pass1 = pass1TextField.text else {return ("", false)}
+        guard let pass2 = pass2TextField.text else {return ("", false)}
+        
+        if email.isEmpty {
+            return ("メールアドレスを入力して下さい", false)
+        }
+        if pass1.isEmpty || pass2.isEmpty {
+            return ("パスワードを入力して下さい", false)
+        }
+        if pass1 != pass2 {
+            return ("パスワードが一致しません", false)
+        }
+        return ("", true)
+    }
 }
 
 extension RegistrationViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn shshouldChangeCharactersInange: NSRange, replacementString string: String) -> Bool {
+        if !errorMessage().1 {
+            label.text = errorMessage().0
+        }
         return true
     }
 }
